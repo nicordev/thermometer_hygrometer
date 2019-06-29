e6805fb693b1c467ce811ddae31c759d7c2f1350
nicordev <nicolas.renvoise.dev@gmail.com>
2019-06-29 20:54:14 +0200

    Show temperature and humidity

--- thermometer_hygrometer.ino
+++ thermometer_hygrometer.ino
@@ -2,21 +2,59 @@
  * A thermometer and a hygrometer with a LCD screen
  */
 #include <LiquidCrystal.h>
+#include <dht_nonblocking.h>
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+// initialize the screen library with the numbers of the interface pins
+LiquidCrystal screen(7, 8, 9, 10, 11, 12);
+
+// Thermometer and hygrometer
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+/*
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_environment(float *temperature, float *humidity)
+{
+    static unsigned long measurement_timestamp = millis();
+
+    /* Measure once every four seconds. */
+    if (millis() - measurement_timestamp > 3000ul)
+    {
+        if (dht_sensor.measure(temperature, humidity) == true)
+        {
+            measurement_timestamp = millis();
+            return (true);
+        }
+    }
+
+    return (false);
+}
 
 void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Hello, World!");
+    // set up the LCD's number of columns and rows:
+    screen.begin(16, 2);
+    screen.print("Hello world!");
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+    float temperature;
+    float humidity;
+
+    /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+    if (measure_environment(&temperature, &humidity) == true)
+    {
+        screen.setCursor(0, 0);
+        screen.print("T = ");
+        screen.print(temperature, 1);
+
+        screen.setCursor(0, 1);
+        screen.print(" *C, H = ");
+        screen.print(humidity, 0);
+        screen.print(" %");
+    }
 }
